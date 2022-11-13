7651acf0f58d484295bc0018044f5696ca79037f
Nixon Correa <ndncorrea@gmail.com>
2022-11-13 22:22:44 +0100

    release

---
+++ Game/Assets/Maps/Cielo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Cielo" tilewidth="80" tileheight="176" tilecount="1" columns="1">
+ <image source="Fondos/NonParallax.png" width="80" height="176"/>
+</tileset>
